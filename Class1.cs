9cd28e4a184f70a32ae155ba7dab4f71ad64ab8e
JoseRomero <joseromerodev@gmail.com>
2022-10-11 10:45:25 +0200

    First upload

---
+++ Class1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wordle
+{
+    public class Wordle
+    {
+        //Miembros
+        private string[] words;
+        private string wordPlay;
+        //private string userDecision;
+
+        // Constructor Wordle estado inicial
+        public Wordle(string wordPlay) 
+        {
+            words = new string[6];
+           // this.UserDecision = "_____BBAR";
+            for (int i = 0; i < words.Length; i++) words[i] = "_____";
+            this.wordPlay = wordPlay.ToUpper();
+        }
+        // Getter setter 
+        public string[] Words
+        {
+            get
+            {
+                return words;
+            }
+            set 
+            {
+                words = value;
+            }
+        }
+        public string WordPlay
+        {
+            get
+            {
+                return this.wordPlay;
+            }
+            set
+            { 
+                this.wordPlay = value;
+            }
+        }
+        //public string UserDecision
+        //{
+        //    get
+        //    {
+        //        return this.userDecision;
+        //    }
+        //    set 
+        //    { 
+
+        //        this.userDecision = value.ToUpper();
+        //    }
+
+        //}
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace Wordle
+{ 
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ResetColor();
+            Wordle wordle = new Wordle("RUEDA");
+            string palabraGanadora = "CAMPO";
+            string palabraComprobar ;
+            bool exit=false;
+            bool win = false;
+            int n = 0;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Adivina la palabra!");
+                for (int k = 0; k < wordle.Words.Length; k++) // RECORRE LA ARRAY DE PALABRAS
+                {
+                    palabraComprobar = wordle.Words[k]; // ASIGANAMOS A PALABRA INTRODUCIDA
+                    for (int i = 0; i < palabraGanadora.Length; i++) 
+                    {
+                        for (int j = 0; j < palabraGanadora.Length; j++)
+                        {
+                            if (palabraComprobar[i] == palabraGanadora[j] && i == j) ColorGreen(); // SI LA LETRA COINCIDE CON LA LETRA Y EN EL MISMO SITIO CAMBIAMOS COLOR
+                            if (palabraComprobar[i] == palabraGanadora[j] && i != j) ColorYellow(); // SI LA LETRA ESTÁ EN LA PALABRA PERO NO EN EL MISMO SITIO CAMBIAMOS COLOR
+                        }
+                        Console.Write($"{palabraComprobar[i]} "); //print letra 
+                        ResetColor();
+                    }
+                    Console.WriteLine();  
+                }
+                
+                for (int i = 0; i < wordle.Words.Length; i++)
+                {
+                    if (wordle.Words[i] == palabraGanadora)
+                    {
+                        exit = true; //RECORREMOS ARRAY PARA VER SI HEMOS DICHO LA PALABRA CORRECTA
+                        win = true;
+                    } 
+                }
+                   
+                if (!exit==true) wordle.Words[n] = Console.ReadLine().ToUpper(); // SI ES CORRECTA LA PALABRA NO PEDIMOS MAS DATOS Y SALIMOS
+                if (n == 6)
+                {
+                    exit = true;
+                    win = false;
+                }
+
+                n++;
+            } while (!exit);
+
+            if (win)
+            {
+                Console.WriteLine("WIN WIN WIN WIN");
+            }
+            else
+            {
+                Console.WriteLine("LOSE LOSE LOSE LOSE");
+            }
+
+            //PrintWords(wordle);
+            //Console.WriteLine("Adivina la palabra!");
+            //wordle.UserDecision = Console.ReadLine(); // decision del usuario
+            //wordle.Words[0] = wordle.UserDecision;
+            //PrintWords(wordle);
+        }
+        //Imprime las palabras  (interfaz de juego)
+       
+        public static void ColorGreen()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+        }
+        public static void ColorYellow()
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+        }
+        public static void ResetColor()
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+        }
+    }
+}
